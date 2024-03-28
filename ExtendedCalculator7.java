42b39345335597e604b82440131880406ed5ee83
01fe22bca064 <01fe22bca064@kletech.ac.in>
2024-03-28 09:32:00 -0700

    code for square and cube function

---
+++ ExtendedCalculator7.java
@@ -0,0 +1,11 @@
+public class ExtendedCalculator7 extends Calculator7 {
+    public float Square(float num)
+    {
+        return(num * num); 
+    }
+    public float Cube(float num)
+    {
+        return(num * num * num); 
+    }
+    
+}
